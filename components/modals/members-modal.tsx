b3c13f0ff07b6218ac93fbee2bd96ba0e2dcf2f4
piercenovo <piercenovoah@gmail.com>
2024-03-25 23:08:14 -0500

    feat: add Manage Members

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,94 +1,178 @@
 'use client'
 
+import qs from 'query-string'
 import axios from 'axios'
-import { Check, Copy, RefreshCw } from 'lucide-react'
 import { useState } from 'react'
 
 import {
   Dialog,
   DialogContent,
   DialogHeader,
-  DialogTitle
+  DialogTitle,
+  DialogDescription
 } from '@/components/ui/dialog'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger
+} from '@/components/ui/dropdown-menu'
 
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { useOrigin } from '@/hooks/use-origin'
 import { useModal } from '@/hooks/use-modal-store'
+import { ServerWithMembersWithProfiles } from '@/types'
+import { UserAvatar } from '@/components/user-avatar'
+import { Check, Gavel, Loader2, MoreVertical, Shield, ShieldCheck, ShieldQuestion } from 'lucide-react'
+import { MemberRole } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className='size-4 ml-2 text-indigo-500' />,
+  ADMIN: <ShieldCheck className='size-4 ml-2 text-rose-500' />
+}
 
 export const MembersModal = () => {
+  const router = useRouter()
   const { onOpen, isOpen, onClose, type, data } = useModal()
-  const origin = useOrigin()
+  const [loadingId, setLoadingId] = useState('')
 
   const isModalOpen = isOpen && type === 'members'
-  const { server } = data
+  const { server } = data as {server: ServerWithMembersWithProfiles }
 
-  const [copied, setCopied] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`
-
-  const onCopy = () => {
-    navigator.clipboard.writeText(inviteUrl)
-    setCopied(true)
-
-    setTimeout(() => {
-      setCopied(false)
-    }, 1000)
-  }
-
-  const onNew = async () => {
+  const onKick = async (memberId: string) => {
     try {
-      setIsLoading(true)
-      const response = await axios.patch(`/api/servers/${server?.id}/invite-code`)
+      setLoadingId(memberId)
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id
+        }
+      })
 
-      onOpen('invite', { server: response.data })
+      const response = await axios.delete(url)
+
+      router.refresh()
+      onOpen('members', { server: response.data })
     } catch (err) {
       console.log(err)
     } finally {
-      setIsLoading(false)
+      setLoadingId('')
+    }
+  }
+
+  const onRoleChange = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId)
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id
+        }
+      })
+
+      const response = await axios.patch(url, { role })
+
+      router.refresh()
+      onOpen('members', { server: response.data })
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoadingId('')
     }
   }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent
-        className='bg-white text-black p-0 overflow-hidden'
+        className='bg-white text-black overflow-hidden'
       >
         <DialogHeader className='pt-8 px-6'>
           <DialogTitle className='text-2xl text-center font-bold'>
-            Invite Friends
+            Manage Members
           </DialogTitle>
+          <DialogDescription
+            className='text-center text-zinc-500'
+          >
+            {server?.members?.length} Members
+          </DialogDescription>
         </DialogHeader>
-        <div className='p-6'>
-          <Label
-            className='uppercase text-sm font-bold text-zinc-500 dark:text-secondary/70'
-          >
-            Server invite link
-          </Label>
-          <div className='flex items-center mt-2 gap-x-2'>
-            <Input
-              disabled={isLoading}
-              className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
-              value={inviteUrl}
-            />
-            <Button disabled={isLoading} onClick={onCopy} size='icon'>
-              {copied ? <Check className='size-4' /> : <Copy className='size-4' />}
-
-            </Button>
-          </div>
-          <Button
-            onClick={onNew}
-            disabled={isLoading}
-            variant='link'
-            size='sm'
-            className='text-xs text-zync-500 mt-4'
-          >
-            Generate a new link
-            <RefreshCw className='size-4 ml-2' />
-          </Button>
-        </div>
+        <ScrollArea className='mt-8 max-h-[420px] pr-6'>
+          {server?.members?.map((member) => (
+            <div key={member.id} className='flex items-center gap-x-2 mb-6'>
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className='flex flex-col gap-y-1'>
+                <div className='text-xs font-semibold flex items-center'>
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className='text-xs text-zinc-500'>
+                  {member.profile.email}
+                </p>
+              </div>
+              {server.profileId !== member.profileId && loadingId !== member.id && (
+                <div className='ml-auto'>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className='size-4 text-zinc-500' />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side='left'>
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger
+                          className='flex items-center'
+                        >
+                          <ShieldQuestion
+                            className='size-4 mr-2'
+                          />
+                          <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem
+                              onClick={() => onRoleChange(member.id, MemberRole.GUEST)}
+                            >
+                              <Shield className='size-4 mr-2' />
+                              Guest
+                              {member.role === MemberRole.GUEST && (
+                                <Check className='size-4 ml-auto' />
+                              )}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => onRoleChange(member.id, MemberRole.MODERATOR)}
+                            >
+                              <ShieldCheck className='size-4 mr-2' />
+                              Moderator
+                              {member.role === MemberRole.MODERATOR && (
+                                <Check className='size-4 ml-auto' />
+                              )}
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem
+                        onClick={() => onKick(member.id)}
+                      >
+                        <Gavel className='size-4 mr-2' />
+                        Kick
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              )}
+              {loadingId === member.id && (
+                <Loader2
+                  className='animate-spin text-zinc-500 ml-auto size-4'
+                />
+              )}
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   )
