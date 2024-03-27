d674468422c3a24476f272e7ddffb9c47ada1103
piercenovo <piercenovoah@gmail.com>
2024-03-27 02:13:31 -0500

    feat: add Emoji Bar

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,8 +5,8 @@ import axios from 'axios'
 import qs from 'query-string'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { Plus } from 'lucide-react'
 
-import { Plus, Smile } from 'lucide-react'
 import {
   Form,
   FormControl,
@@ -15,6 +15,8 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { useModal } from '@/hooks/use-modal-store'
+import { EmojiPicker } from '@/components/emoji-picker'
+import { useRouter } from 'next/navigation'
 
 interface ChatInputProps {
     apiUrl: string
@@ -34,6 +36,7 @@ export const ChatInput = ({
   type
 }: ChatInputProps) => {
   const { onOpen } = useModal()
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -52,6 +55,9 @@ export const ChatInput = ({
       })
 
       await axios.post(url, values)
+
+      form.reset()
+      router.refresh()
     } catch (err) {
       console.log(err)
     }
@@ -81,7 +87,9 @@ export const ChatInput = ({
                     {...field}
                   />
                   <div className='absolute top-7 right-8'>
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) => field.onChange(`${field.value}${emoji}`)}
+                    />
                   </div>
                 </div>
               </FormControl>
