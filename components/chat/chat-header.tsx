2c2e5658b0598fd8b1f32c7bea88f8c85cfe6c7c
piercenovo <piercenovoah@gmail.com>
2024-03-28 01:18:27 -0500

    feat: add Video Calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from 'lucide-react'
 import { MobileToggle } from '@/components/mobile-toggle'
 import { UserAvatar } from '@/components/user-avatar'
 import { SocketIndicator } from '@/components/socket-indicator'
+import { ChatVideoButton } from '@/components/chat/chat-video-button'
 
 interface ChatHeaderProps {
     serverId: string
@@ -37,6 +38,9 @@ export const ChatHeader = ({
       </p>
 
       <div className='ml-auto flex items-center '>
+        {type === 'conversation' && (
+          <ChatVideoButton />
+        )}
         <SocketIndicator />
       </div>
     </div>
